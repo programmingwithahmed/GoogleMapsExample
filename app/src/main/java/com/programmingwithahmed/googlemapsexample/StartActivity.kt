1c17bcd567e330804ccc7cf946c4924dc84246a7
Ahmed <programmingwithahmed2020@gmail.com>
2021-02-16 11:09:39 +0300

    add start page

---
+++ app/src/main/java/com/programmingwithahmed/googlemapsexample/StartActivity.kt
@@ -0,0 +1,31 @@
+package com.programmingwithahmed.googlemapsexample
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.programmingwithahmed.googlemapsexample.databinding.ActivityStartBinding
+
+class StartActivity : AppCompatActivity() {
+
+    lateinit var binding: ActivityStartBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = ActivityStartBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+
+        binding.apply {
+
+            btnMapWithMarker.setOnClickListener {
+
+                val intent = Intent(this@StartActivity, MapsActivity::class.java)
+                startActivity(intent)
+
+            }
+
+
+        }
+    }
+}
